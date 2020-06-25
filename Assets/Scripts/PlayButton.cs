3e0bd127bb589fa8eec052c443de16537313d99a
Smashface <51160929+Smashface90@users.noreply.github.com>
2020-06-25 10:36:54 +0800

    evan thurs 1

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿//Script by Evan Waters 1017144
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+   public void PlayButt()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
