03be7ff631fe4d8245c7c0085ad29b77160a17c2
Smashface <51160929+Smashface90@users.noreply.github.com>
2020-06-25 20:39:55 +0800

    evan thurs last

--- Assets/Scripts/PlayerScrip.cs
+++ Assets/Scripts/PlayerScrip.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerScrip : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Player Swap")]
+    public GameObject leftC;
+    public GameObject rightC;
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+            Debug.Log("test");
+                if (leftC.activeInHierarchy == true)
+                {
+                    rightC.SetActive(true);
+                    leftC.SetActive(false);
+                Debug.Log("test2");
+            }
+
+            
+
+                else
+                {
+                    rightC.SetActive(false);
+                    leftC.SetActive(true);
+                Debug.Log("Else");
+                }
+            }
+        }
     }
 }
