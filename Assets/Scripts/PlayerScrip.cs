1b8fab2c4e6a1ca60861d10c234c6ab565a0edad
Smashface <51160929+Smashface90@users.noreply.github.com>
2020-06-30 10:19:05 +0800

    tues update

--- Assets/Scripts/PlayerScrip.cs
+++ Assets/Scripts/PlayerScrip.cs
@@ -12,18 +12,33 @@ public class PlayerScrip : MonoBehaviour
     [Header("Level")]
     public float camSpeed;
     public Text levelNum;
+    public float jumpTime;
+
+    [Header("Health")]
+    public Image HPBar;
+    public float Health;
 
     public void Start()
     {
+        //level text starts at 1
         SetLevelText(1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        jumpTime -= Time.deltaTime;
 
-        //camera transform
+        //camera transform. constantly moving down with camSpeed to adjust speed per lvl
         transform.Translate(Vector2.down * camSpeed * Time.deltaTime, Camera.main.transform);
+
+        //health stuff
+        HPBar.fillAmount = Health / 100;
+            //NOTES:
+               //if collision.tag "player" collides with collision.tag "enemy"
+              // -25 Health
+             //if Health <= 0
+            //pause, show game over text, save score, go to main menu
 
         //tap controls
         for (int i = 0; i < Input.touchCount; ++i)
@@ -34,16 +49,15 @@ public class PlayerScrip : MonoBehaviour
                 if (leftC.activeInHierarchy == true)
                 {
                     //NOTE:add wait 0.2f and animation
+                    
                     rightC.SetActive(true);
                     leftC.SetActive(false);
-                Debug.Log("test2");
             }           
 
                 else
                 {
                     rightC.SetActive(false);
                     leftC.SetActive(true);
-                Debug.Log("Else");
                 }
             }
         }
